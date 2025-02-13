823426d83f2b08c844d6172997f1ae4a5b618cf9
RoyLin <orangeskyonline@gmail.com>
2025-02-13 14:06:12 +0800

    製作新增商品分類功能

--- back/th.php
+++ back/th.php
@@ -3,13 +3,13 @@
 <div class="ct">
     新增大分類
     <input type="text" name="big" id="big">
-    <button>新增</button>
+    <button onclick="addType('big')">新增</button>
 </div>
 <div class="ct">
     新增中分類
-    <select name="selbig" id="selbit"></select>
+    <select name="selbig" id="selbig"></select>
     <input type="text" name="mid" id="mid">
-    <button>新增</button>
+    <button onclick="addType('mid')">新增</button>
 </div>
 
 <table class="all">
@@ -22,12 +22,37 @@
         <td class="pp"><button>修改</button><button>刪除</button></td>
     </tr>
     <tr class='ct'>
-        <td class="pp">男用皮件尸</td>
+        <td class="pp">男用皮件</td>
         <td class="pp"><button>修改</button><button>刪除</button></td>
     </tr>
 </table>
 
+<script>
+getBigs();
 
+function addType(type){
+    let name,big_id;
+    switch(type){
+        case 'big':
+            name=$("#big").val();
+            big_id=0;
+            break;
+        case 'mid':
+            name=$("#mid").val();   
+            bigId=$("#selbig").val();
+        }
+            $.post("./api/add_types.php",{name,big_id},function(){
+                location.reload();
+            })
+}
+
+function getBigs(){
+    $.get("./api/add_types.php",function(bigs){
+        $("$selbig").html
+    })
+}
+
+</script>
 
 
 <h2 class="ct">商品管理</h2>
@@ -44,8 +69,8 @@
         <td class="ct">操作</td>
     </tr>
     <tr class="pp">
-        <td class="ct"></td>
-        <td></td>
+        <td class="ct">1</td>
+        <td>2</td>
         <td class="ct"></td>
         <td class="ct"></td>
         <td class="ct"></td>
